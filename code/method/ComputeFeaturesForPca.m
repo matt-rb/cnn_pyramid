8b7b4161daf3bd52823471079ffff03ace4da401
Mahadyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-08-28 21:44:28 +0200

    Speedup ComputeFeatures

--- code/method/ComputeFeaturesForPca.m
+++ code/method/ComputeFeaturesForPca.m
@@ -14,6 +14,7 @@ else
 end
 
 dispstat ('','init');
+cnn_feature_size = cell(length(Dataall),1);
 for categorynum =1:length(Dataall)%-------------------------extract feature for whole of dataset
     for vidnum = 1: size(Dataall{categorynum},1)%-------------------------extract feature for each video
         data = Dataall{categorynum}{vidnum};
