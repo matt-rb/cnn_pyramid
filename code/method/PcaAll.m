5138217a9d72bcbace185131fbf0df1967d70ced
Mahadyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-08-30 11:25:43 +0200

    Speedup Appliyng pca

--- code/method/PcaAll.m
+++ code/method/PcaAll.m
@@ -5,14 +5,17 @@ end
 dispstat ('','init');
 for categorynum =1:length(Dataall)%-------------------------extract feature for whole of dataset
     for vidnum = 1: size(Dataall{categorynum},1)%-------------------------extract feature for each video
-        Data_pca = [];
+        
         Fname1 = ['cat-[' num2str(categorynum) ']-video-[' num2str(vidnum)  ']'];
         load(fullfile(options.output,options.run_name,Fname1),'cnn_feature_video');
         dispstat (['Apply PCA for: ' Fname1]);
         numf = size(cnn_feature_video,2)/options.legcnn;
+        Data_pca = zeros(size(cnn_feature_video,2), numf*options.rdim);
+        %Data_pca = [];
         for jj =1:numf
-            pX=cnn_feature_video(:,((jj-1)*options.legcnn+1):(jj*options.legcnn))*v_pca{jj};
-            Data_pca = [Data_pca,cnn_feature_video(:,((jj-1)*options.legcnn+1):(jj*options.legcnn))*v_pca{jj}];
+            %pX=cnn_feature_video(:,((jj-1)*options.legcnn+1):(jj*options.legcnn))*v_pca{jj};
+            %Data_pca = [Data_pca,cnn_feature_video(:,((jj-1)*options.legcnn+1):(jj*options.legcnn))*v_pca{jj}];
+            Data_pca(:,(jj-1)*options.rdim+1:options.rdim*jj) = cnn_feature_video(:,((jj-1)*options.legcnn+1):(jj*options.legcnn))*v_pca{jj};
         end
         Fname1 = ['cat-[' num2str(categorynum) ']-video-[' num2str(vidnum)  ']'];
         save(fullfile(options.output,options.run_name,Fname1),'cnn_feature_video','Data_pca')
