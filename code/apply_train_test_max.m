6edca6f83afc4c3f74519bed18e882487533e7d7
Mahadyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-09-06 03:43:33 +0200

    Update MaximaFeatures using gaussian

--- code/apply_train_test_max.m
+++ code/apply_train_test_max.m
@@ -5,22 +5,35 @@ disp('Spelitting Test/Train Data ...');
 [TestData,TrainData,indTest1,indTrain1] = Splitting(feats,test_train_idx);
 
 % --PCA
-if options.apply_PCA        
+if options.apply_PCA
     disp('Apply PCA ...');
     [TrainData_pca,TestData_pca] = PcaData(TrainData,TestData,options);
 end
 
 
-% --Create Label 
+% --Create Label
 disp('Creat Test/Train Labels ...');
 [train_label,test_label] = CreateLabel(indTrain1,indTest1);
 
-% --Training
-disp('Training ...');
-[classifiers_linear] = Traing1(TrainData,train_label,options);
-% --Testing
-disp('Testing ...');
-[acc_orginal,confusion_linear] = Testing1(TestData,test_label,classifiers_linear,options);
+
+if options.apply_PCA
+    % --Training
+    disp('Training ...');
+    [classifiers_linear] = Traing1(TrainData_pca,train_label,options);
+    % --Testing
+    disp('Testing ...');
+    [acc_orginal,confusion_linear] = Testing1(TestData_pca,test_label,classifiers_linear,options);
+    
+else
+    % --Training
+    disp('Training ...');
+    [classifiers_linear] = Traing1(TrainData,train_label,options);
+    % --Testing
+    disp('Testing ...');
+    [acc_orginal,confusion_linear] = Testing1(TestData,test_label,classifiers_linear,options);
+    
+end
+
 % --Evaluation
 disp(['Accuracy: ' num2str(acc_orginal)]);
 % create_confusion_matrix;
--- code/method/ComputeMaximasFeature.m
+++ code/method/ComputeMaximasFeature.m
@@ -12,7 +12,7 @@ tic;
 for sample_idx = 1 : all_video_no
     %feat = zeros(1,feat_dim);
     features_video = Dataall{indexDataall{sample_idx,1}}{indexDataall{sample_idx,2}};
-    feat = comp_feats(features_video, options.maxima_npeaks, options);
+    feat = comp_feats2(features_video, options.maxima_npeaks, options);
     %for dim_no = 1: options.legcnn
     %    tt = get_n_peaks(features_video(dim_no,:), options.maxima_npeaks);
     %    feat(((dim_no-1)*options.maxima_npeaks)+1:options.maxima_npeaks*dim_no) = tt;
@@ -25,30 +25,10 @@ disp (num2str(toc));
 end
 
 function feat = comp_feats(features_video, n_peaks, options)
-    feat_dim = options.legcnn * options.maxima_npeaks;
-    feat = zeros(1,feat_dim);
-    for dim_no = 1: options.legcnn
-        [val,idx] = findpeaks(features_video(dim_no,:),'SortStr','descend','minpeakdistance',3,'npeaks',n_peaks);
-        maximas = zeros(1,n_peaks);
-        if ~isempty(val)
-            % make index/value for peaks
-            maximas_temp = [idx; val];
-            % sort peaks by index
-            [~,I]=sort(maximas_temp(1,:));
-            % Exclude index row
-            maximas(1:length(val)) = maximas_temp(2,I);
-        end
-        feat(((dim_no-1)*options.maxima_npeaks)+1:options.maxima_npeaks*dim_no) = maximas;
-    end
-end
-
-function maximas = get_n_peaks(feats_vector, n_peaks)
-
-    % features_video N x dim
-    %regmax = imregionalmax(feats_vector);
-    %tt = regmax*.feats_vector;
-    
-    [val,idx] = findpeaks(feats_vector,'SortStr','descend','minpeakdistance',3,'npeaks',n_peaks);
+feat_dim = options.legcnn * options.maxima_npeaks;
+feat = zeros(1,feat_dim);
+for dim_no = 1: options.legcnn
+    [val,idx] = findpeaks(features_video(dim_no,:),'SortStr','descend','minpeakdistance',3,'npeaks',n_peaks);
     maximas = zeros(1,n_peaks);
     if ~isempty(val)
         % make index/value for peaks
@@ -58,4 +38,32 @@ function maximas = get_n_peaks(feats_vector, n_peaks)
         % Exclude index row
         maximas(1:length(val)) = maximas_temp(2,I);
     end
+    feat(((dim_no-1)*options.maxima_npeaks)+1:options.maxima_npeaks*dim_no) = maximas;
+end
+end
+
+function feats_filtered = apply_gaussian (features_video)
+    sigma = 5;
+    size = 30;
+    x = linspace(-size / 2, size / 2, size);
+    gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+    gaussFilter = gaussFilter / sum (gaussFilter); % normalize
+    feats_filtered = conv2 (features_video, gaussFilter, 'same');
+end
+
+function feats = comp_feats2(feats_vector, n_peaks, options)
+
+% features_video N x dim
+feats_filtered = apply_gaussian (feats_vector);
+regmax = imregionalmax(feats_filtered);
+tt = regmax.*feats_vector;
+[val,ind]=sort(tt,2,'descend');
+val = val(:,1:3);
+ind_selected = ind(:,1:3);
+[val_s, ind_s] = sort(ind_selected,2);
+
+for i=1: size(feats_vector,1)
+   val_s(i,:)= val(i, ind_s(i,:));
+end
+feats = reshape(val_s',1,4096*3);
 end
